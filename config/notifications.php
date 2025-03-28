422884a6ac5d1e303afa0af4abcf9fc2a2176349
Opeyemi Ilesanmi <ojilesanmi7@gmail.com>
2025-03-28 20:14:08 +0100

    batch job

---
+++ config/notifications.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Stakeholders Emails
+|--------------------------------------------------------------------------
+| Specify the stakeholders emails that should be notified of the export
+| completion.
+|
+*/
+
+return [
+  'export_recipients' => ['ojilesanmi7@gmail.com'],
+];
